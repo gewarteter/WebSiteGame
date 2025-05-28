ac0988b5dcff0291dfb0193422afbc9ef0312b40
gewarteter <ankakotik74@gmail.com>
2025-05-28 21:30:05 +0400

    Добавлены контроллеры

---
+++ Controllers/LevelsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcGame.Controllers
+{
+    public class LevelsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
