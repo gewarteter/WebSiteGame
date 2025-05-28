71c0d961c19fa65be19fac94a69f56687d06cc1e
gewarteter <ankakotik74@gmail.com>
2025-05-29 00:42:25 +0400

    Созданы страницы без стилизации и текста

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace MvcGame.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
